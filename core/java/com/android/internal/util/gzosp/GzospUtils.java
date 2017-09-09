cd3ac0b7fb97c2a0526db1b69e688a33af0e701c
ezio84 <brabus84@gmail.com>
2017-09-09 22:15:04 +0800

    Add a sendKeycode api to GzospUtils code borrowed from com.android.systemui.statusbar.policy.KeyButtonView

--- core/java/com/android/internal/util/gzosp/GzospUtils.java
+++ core/java/com/android/internal/util/gzosp/GzospUtils.java
@@ -21,6 +21,9 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.hardware.input.InputManager;
+import android.os.Handler;
+import android.os.Looper;
 import android.content.res.Resources;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
@@ -32,6 +35,9 @@ import android.view.DisplayInfo;
 import android.view.WindowManager;
 import android.view.IWindowManager;
 import android.view.WindowManagerGlobal;
+import android.view.InputDevice;
+import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
 
 import java.util.Locale;
 
@@ -127,6 +133,33 @@ public class GzospUtils {
     public static boolean isTablet(Context context) {
         return getScreenType(context) == DEVICE_TABLET;
     }
+
+
+    public static void sendKeycode(int keycode) {
+        long when = SystemClock.uptimeMillis();
+        final KeyEvent evDown = new KeyEvent(when, when, KeyEvent.ACTION_DOWN, keycode, 0,
+                0, KeyCharacterMap.VIRTUAL_KEYBOARD, 0,
+                KeyEvent.FLAG_FROM_SYSTEM | KeyEvent.FLAG_VIRTUAL_HARD_KEY,
+                InputDevice.SOURCE_KEYBOARD);
+        final KeyEvent evUp = KeyEvent.changeAction(evDown, KeyEvent.ACTION_UP);
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                InputManager.getInstance().injectInputEvent(evDown,
+                        InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
+            }
+        });
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                InputManager.getInstance().injectInputEvent(evUp,
+                        InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
+            }
+        }, 20);
+    }
+
 
     // Omni Switch Constants
 
